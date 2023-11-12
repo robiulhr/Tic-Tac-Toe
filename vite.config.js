695661671dcee326f400d45acaf8e04e88e258f9
robiulhr <rh22012001@gmail.com>
2023-11-12 22:08:30 +0600

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "Tic-Tac-Toe",
   plugins: [react()],
   optimizeDeps: {
     exclude: ['js-big-decimal']
